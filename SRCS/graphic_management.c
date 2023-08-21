c305e3b76922310fcb1aab9c4a040a0e2210d29a
lgomez-g <you@example.com>
2023-08-21 15:18:04 +0100

    listo

--- SRCS/graphic_management.c
+++ SRCS/graphic_management.c
@@ -6,7 +6,7 @@
 /*   By: franciscogomez <franciscogomez@student.42. +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 18:31:06 by franciscogomez    #+#    #+#             */
-/*   Updated: 2023/08/21 14:13:24 by franciscogomez   ###   ########.fr       */
+/*   Updated: 2023/08/21 15:14:22 by franciscogomez   ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,45 +27,60 @@ void handle_key(int keycode, t_game *game)
         mlx_destroy_window(game->mlx, game->windows);
         exit(0);
     }
-    else if (keycode == 119) // Tecla W (arriba)
+}
+else if (keycode == 119) // Tecla W (arriba)
+{
+    if (game->map[game->player_y - 1][game->player_x] != '1')
     {
-        if (game->map[game->player_y - 1][game->player_x] != '1')
+        // Verifica si la celda contiene un objeto coleccionable
+        if (game->map[game->player_y - 1][game->player_x] == 'C')
         {
-            game->player_y--;
-            game->movements++;
-            printf("Movimientos: %d\n", game->movements); // Muestra el número de movimientos
+            game->map[game->player_y - 1][game->player_x] = '0'; // Cambia el objeto coleccionable por un espacio en blanco
+            game->collected_objects++; // Incrementa el contador de objetos recolectados
         }
+        game->player_y--;
+        game->movements++;
     }
-    else if (keycode == 115) // Tecla S (abajo)
+}
+else if (keycode == 115) // Tecla S (abajo)
+{
+    if (game->map[game->player_y + 1][game->player_x] != '1')
     {
-        if (game->map[game->player_y + 1][game->player_x] != '1')
+        if (game->map[game->player_y + 1][game->player_x] == 'C')
         {
-            game->player_y++;
-            game->movements++;
-            
-            printf("Movimientos: %d\n", game->movements); // Muestra el número de movimientos
+            game->map[game->player_y + 1][game->player_x] = '0'; 
+            game->collected_objects++; 
+                
         }
+        game->player_y++;
+        game->movements++;
     }
-    else if (keycode == 97) // Tecla A (izquierda)
-    {
+}
+else if (keycode == 97) // Tecla A (izquierda)
+{
         if (game->map[game->player_y][game->player_x - 1] != '1')
         {
+            if (game->map[game->player_y][game->player_x - 1] == 'C')
+            {
+                game->map[game->player_y][game->player_x - 1] = '0'; 
+                game->collected_objects++; 
+            }
             game->player_x--;
             game->movements++;
-            
-            printf("Movimientos: %d\n", game->movements); // Muestra el número de movimientos
         }
-    }
-    else if (keycode == 100) // Tecla D (derecha)
-    {
+}
+else if (keycode == 100) // Tecla D (derecha)
+{
         if (game->map[game->player_y][game->player_x + 1] != '1')
         {
+            if (game->map[game->player_y][game->player_x + 1] == 'C')
+            {
+                game->map[game->player_y][game->player_x + 1] = '0'; 
+                game->collected_objects++; 
+            }
             game->player_x++;
             game->movements++;
-            
-            printf("Movimientos: %d\n", game->movements); // Muestra el número de movimientos
         }
-    }
 }
 
 
